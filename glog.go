719e20ad292ae5c269be548c6dea9ec22d290ce2
daCheng <xiangxiaoc@outlook.com>
2024-11-19 14:28:25 +0800

    Print LOG_LEVEL before set default logger to slog

--- glog.go
+++ glog.go
@@ -1,28 +1,28 @@
 package glog
 
 import (
-	"fmt"
+	"log"
 	"log/slog"
 	"os"
 	"strings"
 	"syscall"
 )
 
-const defaultLevel = "info"
+const defaultLevel = "INFO"
 
 const envName = "LOG_LEVEL"
 
 var levelMap = map[string]slog.Level{
-	"debug": slog.LevelDebug,
-	"info":  slog.LevelInfo,
-	"warn":  slog.LevelWarn,
-	"error": slog.LevelError,
+	"DEBUG": slog.LevelDebug,
+	"INFO":  slog.LevelInfo,
+	"WARN":  slog.LevelWarn,
+	"ERROR": slog.LevelError,
 }
 
 // getLogLevel returns the log level from the string
 // 从给定的字符串中获取日志级别
 func getLogLevel(l string) slog.Level {
-	return levelMap[strings.ToLower(l)]
+	return levelMap[strings.ToUpper(l)]
 }
 
 // init import this package to initialize the logger and setup slog
@@ -34,22 +34,7 @@ func init() {
 	handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
 		AddSource: true, Level: getLogLevel(level),
 	})
+	log.Printf("Finished to setup slog, LOG_LEVEL is %s \n", strings.ToUpper(level))
 	logger := slog.New(handler)
 	slog.SetDefault(logger)
-}
-
-func Debug(v any) {
-	slog.Debug(fmt.Sprint(v))
-}
-
-func Info(v any) {
-	slog.Info(fmt.Sprint(v))
-}
-
-func Warn(v any) {
-	slog.Warn(fmt.Sprint(v))
-}
-
-func Error(v any) {
-	slog.Error(fmt.Sprint(v))
 }
